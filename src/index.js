2b018a114f774f1d7954a9d50824691d2d4d9bde
guomei <lengxuehx@gmail.com>
2018-04-08 13:27:07 +0800

    creact ELcomponent and renders the accordion panel-group well

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Panel, PanelGroup } from 'react-bootstrap';
+import { Panel, PanelGroup, Accordion } from 'react-bootstrap';
+
+
+class ELComponent extends React.Component {
+  render(){
+    return (
+      <PanelGroup accordion id="board">
+        <Panel eventKey="1">
+          <Panel.Heading>
+            <Panel.Title toggle>Collapsible Group Item #1</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            Anim pariatur cliche reprehenderit,
+          </Panel.Body>
+        </Panel>
+        <Panel eventKey="2">
+          <Panel.Heading>
+            <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            Anim pariatur cliche reprehenderit, enim eiusmod
+          </Panel.Body>
+        </Panel>
+        <Panel eventKey="3">
+          <Panel.Heading>
+            <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            Anim pariatur cliche reprehenderit,
+          </Panel.Body>
+        </Panel>
+      </PanelGroup>
+    );
+  }
+}
+ReactDOM.render(<ELComponent/>, document.getElementById('root'));
