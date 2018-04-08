1ca789ff40df16df1fae0fb2924193c8f1498084
guomei <lengxuehx@gmail.com>
2018-04-08 15:47:31 +0800

    install jest-css-modules and config

--- src/ELComponent.test.jsx
+++ src/ELComponent.test.jsx
@@ -1,4 +1,19 @@
+import React from 'react';
+import { expect } from 'chai';
 import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import ELComponent from './ELComponent';
+
 Enzyme.configure({ adapter: new Adapter() });
+
+
+describe('<ELComponent />', () => {
+  it('check employee component count', () => {
+    const wrapper = shallow( < ELComponent / > );
+    let employeeCount = wrapper.state().employees.length;
+    expect(wrapper.find('#board').shallow().find('.EmployeeComponent')).to.have.length(employeeCount);
+  });
+
+});
