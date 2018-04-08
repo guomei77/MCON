277d9f47d4ac15cd0b1f3f76adbd3d1820f65758
guomei <lengxuehx@gmail.com>
2018-04-08 14:05:49 +0800

    implement ELComponent with data

--- src/index.js
+++ src/index.js
@@ -3,37 +3,65 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Panel, PanelGroup, Accordion } from 'react-bootstrap';
 
+// for reusable puropse, extract EmployeeComponent from ELComponent
+function EmployeeComponent(props) {
+  return (
+    <Panel eventKey={props.value} bsStyle="info">
+      <Panel.Heading >
+        <Panel.Title toggle>
+         {props.value.name}
+        </Panel.Title>
+      </Panel.Heading >
+      <Panel.Body collapsible onClick={() => alert(`works in ${props.value.location}`)}>
+        {props.value.position} from {props.value.country}
+      </Panel.Body>
+    </Panel >
+  );
+}
 
 class ELComponent extends React.Component {
-  render(){
+  constructor() {
+    super();
+    {/* data in this.state may also fetch from DB*/}
+    this.state={
+      employees: [{
+        name: 'Sebastien C.',
+        position: 'Developer',
+        country: 'France',
+        location: 'Beijing',
+        id: 1,
+      },
+      {
+        name: 'Guangjie C.',
+        position: 'Developer',
+        country: 'Singapore',
+        location: 'Shanghai',
+        id: 2,
+      },
+      {
+        name: 'Hannes S.',
+        position: 'Developer',
+        country: 'Germany',
+        location: 'Beijing',
+        id: 3,
+      },
+      ],
+    };
+  }
+
+  // render a EmployeeComponent
+  renderEmployee(employeeInfo) {
+    return (<EmployeeComponent key={employeeInfo.id} className="EmployeeComponent" value={employeeInfo}/>);
+  }
+
+  render() {
     return (
-      <PanelGroup accordion id="board">
-        <Panel eventKey="1">
-          <Panel.Heading>
-            <Panel.Title toggle>Collapsible Group Item #1</Panel.Title>
-          </Panel.Heading>
-          <Panel.Body collapsible>
-            Anim pariatur cliche reprehenderit,
-          </Panel.Body>
-        </Panel>
-        <Panel eventKey="2">
-          <Panel.Heading>
-            <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
-          </Panel.Heading>
-          <Panel.Body collapsible>
-            Anim pariatur cliche reprehenderit, enim eiusmod
-          </Panel.Body>
-        </Panel>
-        <Panel eventKey="3">
-          <Panel.Heading>
-            <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
-          </Panel.Heading>
-          <Panel.Body collapsible>
-            Anim pariatur cliche reprehenderit,
-          </Panel.Body>
-        </Panel>
+      <PanelGroup accordion id="board" > { /* insert EmployeeComponent into board according to the employees count */ }
+          {
+            this.state.employees.map(item => (this.renderEmployee(item)))
+          }
       </PanelGroup>
-    );
+   );
   }
 }
 ReactDOM.render(<ELComponent/>, document.getElementById('root'));
